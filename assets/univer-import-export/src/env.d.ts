5c73bb99c49387114b9db4681a52c74e0fa732b7
Liu Chang <liuchang_m@163.com>
2025-08-12 10:26:24 +0800

    for dev

---
+++ assets/univer-import-export/src/env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="vite/client" />
+/// <reference types="@univerjs/vite-plugin/types" />
+
+declare module '@chang-code-hub/luckyexcel-fixed';
---
+++ assets/univer-import-export/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { univerPlugin } from '@univerjs/vite-plugin';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), univerPlugin()],
+  resolve: {
+    alias: {
+      '@chang-code-hub/luckyexcel-fixed': '/Projects/Self/luckyexcel-fixed/dist/luckyexcel.esm.js',
+    },
+  },
+});
